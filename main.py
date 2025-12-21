c5d6cabc739b1a4ed27a96da19948d63688782ff
LeanderStern <rednael3@freenet.de>
2025-12-21 22:24:37 +0100

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import logging
 from api_service.api_service import ApiService
-from api_service.utils.create_env_file import create_env_file
 from base_model import WBBaseModel
 
 def main():
-    create_env_file(
-        {"SYNOPTIC_API_KEY": "your_api_key_here"},
-        file_path= __file__.parent / ".env")
+    api_service = ApiService()
+    response = api_service.get_snow_forecast()
+    print(response.json())
 
 
 if __name__ == "__main__":
-    WBBaseModel.LOGGER.setLevel(logging.INFO)
+    WBBaseModel.LOGGER.setLevel(logging.DEBUG)
     main()
